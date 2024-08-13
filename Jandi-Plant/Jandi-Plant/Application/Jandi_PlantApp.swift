a4416d5eeecf2e0fd3580cbbc16e4d145a4c3dd7
seungchan2 <chanchu@hansung.ac.kr>
2024-08-13 16:25:24 +0900

    add: Application 추가

---
+++ Jandi-Plant/Jandi-Plant/Application/Jandi_PlantApp.swift
@@ -0,0 +1,25 @@
+//
+//  Jandi_PlantApp.swift
+//  Jandi-Plant
+//
+//  Created by MEGA_Mac on 8/2/24.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+@main
+struct Jandi_PlantApp: App {
+    init() {
+        FontType.install()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            RootView(store: Store(initialState: AppReducer.State(),
+                                  reducer: { AppReducer() })
+            )
+        }
+    }
+}
