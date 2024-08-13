9f9887daf1fe38dbe750095f3ad8423ab0545a07
seungchan2 <chanchu@hansung.ac.kr>
2024-08-13 16:26:39 +0900

    feat: 로그인 구현

---
+++ Jandi-Plant/Jandi-Plant/View/Login/GithubLoginView.swift
@@ -0,0 +1,32 @@
+//
+//  LoginView.swift
+//  Jandi-Plant
+//
+//  Created by MEGA_Mac on 8/2/24.
+//
+
+import Combine
+import SwiftUI
+
+import ComposableArchitecture
+
+struct GithubLoginView: View {
+    let store: StoreOf<LoginReducer>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                if viewStore.isLoggedIn {
+                    Text("Logged In")
+                } else {
+                    Button("Login with GitHub") {
+                        viewStore.send(.loginButtonTapped)
+                    }
+                }
+            }
+            .onOpenURL { url in
+                viewStore.send(.handleOpenURL(url))
+            }
+        }
+    }
+}
