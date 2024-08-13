0740fb9904dfe466e1014cf9de4d772ef2efdbd9
seungchan2 <chanchu@hansung.ac.kr>
2024-08-13 16:26:55 +0900

    feat: 세팅 구현

---
+++ Jandi-Plant/Jandi-Plant/View/Setting/SettingView.swift
@@ -0,0 +1,16 @@
+//
+//  SettingView.swift
+//  Jandi-Plant
+//
+//  Created by MEGA_Mac on 8/7/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+            .font(.largeTitle)
+            .navigationBarTitle("Settings", displayMode: .inline)
+    }
+}
