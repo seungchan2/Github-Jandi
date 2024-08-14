a8464ba78de3942d400ebbb94935180dd40d3533
seungchan2 <chanchu@hansung.ac.kr>
2024-08-14 17:25:18 +0900

    test: 테스트 코드 수정 필요

--- Jandi-Plant/Jandi-PlantTests/Jandi_PlantTests.swift
+++ Jandi-Plant/Jandi-PlantTests/Jandi_PlantTests.swift
@@ -5,32 +5,127 @@
 //  Created by MEGA_Mac on 8/2/24.
 //
 
+import Combine
 import XCTest
-@testable import Jandi_Plant
 
-final class Jandi_PlantTests: XCTestCase {
+import Core
+import Dependencies
+import JandiNetwork
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+import ComposableArchitecture
+
+final class HomeReducerTests: XCTestCase {
+    
+    func test_onAppear_fetchCommitsSuccess() async throws {
+//        // Given
+//        let mockCommits: [Commit] = [
+//            Commit(date: Date(), level: .low),
+//            Commit(date: Date(), level: .medium)
+//        ]
+//        
+//        var githubServiceMock = GithubServiceMock(apiService: APIService())
+//        githubServiceMock.mockCommits = mockCommits
+//        
+//        
+//        let store = TestStore(
+//            initialState: HomeReducer.State(),
+//            reducer: { HomeReducer() },
+//            withDependencies: {
+//                $0.githubService = mockService
+//            }
+//        )
+//        
+//        store.dependencies.githubService = mockService
+//        
+//        // When
+//        await store.send(.onAppear) {
+//            $0.isLoading = true
+//        }
+//        
+//        // Then
+//        await store.receive(.getCommitsResponse(.success(mockCommits))) {
+//            $0.isLoading = false
+//            $0.commits = mockCommits
+//        }
+//        
+//        await store.receive(.updateCurrentMonthCommitsCount) {
+//            $0.currentMonthCommitsCount = mockCommits.count
+//            JandiUserDefault.coin = mockCommits.count
+//        }
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
+//    func test_onAppear_fetchCommitsFailure() async throws {
+//        // Given
+//        let mockError = APIError.invalidURL
+//        let githubServiceMock = GithubServiceMock(apiService: APIService())
+//        githubServiceMock.mockError = mockError
+//        
+//        
+//        let store = TestStore(
+//            initialState: HomeReducer.State(),
+//            reducer: { HomeReducer() },
+//            withDependencies: {
+//                $0.githubService = mockService
+//            }
+//        )
+//        
+//        // Inject mock service
+//        store.dependencies.githubService = mockService
+//        
+//        // When
+//        await store.send(.onAppear) {
+//            $0.isLoading = true
+//        }
+//        
+//        // Then
+//        await store.receive(.getCommitsResponse(.failure(mockError))) {
+//            $0.isLoading = false
+//            $0.errorMessage = mockError.localizedDescription
+//        }
+//    }
+    //    // Test failure when fetching commits
+    //    func test_getCommitsResponse_failure() async throws {
+    //        // Given
+    //        let mockError = APIError.networkError
+    //        let store = TestStore(
+    //            initialState: HomeReducer.State(),
+    //            reducer: HomeReducer()
+    //        )
+    //
+    //        store.dependencies.githubService.getCommits = { _, _ in
+    //            Effect(error: mockError)
+    //        }
+    //
+    //        // When
+    //        await store.send(.onAppear) {
+    //            $0.isLoading = true
+    //        }
+    //
+    //        // Then
+    //        await store.receive(.getCommitsResponse(.failure(mockError))) {
+    //            $0.isLoading = false
+    //            $0.errorMessage = mockError.localizedDescription
+    //        }
+    //    }
+    //
+    //    // Test updateCurrentMonthCommitsCount action
+    //    func test_updateCurrentMonthCommitsCount() async throws {
+    //        // Given
+    //        let commits: [Commit] = [
+    //            Commit(date: Date(), level: .low),
+    //            Commit(date: Date(), level: .none),
+    //            Commit(date: Date(), level: .medium)
+    //        ]
+    //
+    //        let store = TestStore(
+    //            initialState: HomeReducer.State(commits: commits),
+    //            reducer: HomeReducer()
+    //        )
+    //
+    //        // When
+    //        await store.send(.updateCurrentMonthCommitsCount) {
+    //            $0.currentMonthCommitsCount = 2 // Only 2 commits with level != .none
+    //            JandiUserDefault.coin = 2
+    //        }
+    //    }
 }
